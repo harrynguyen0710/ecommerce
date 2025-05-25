0c66773a6261c901c1aaabc71274bc796e860c46
harrynguyen0710 <nnhoang0710@gmail.com>
2025-05-25 16:27:26 +0700

    feat: added inventory creation

---
+++ services/discounts/src/middlewares/validateInput.js
@@ -0,0 +1,34 @@
+const Joi = require("joi");
+
+const discountSchema = Joi.object({
+  code: Joi.string().required(),
+  type: Joi.string().valid("PERCENTAGE", "FIXED").required(),
+  value: Joi.number().positive().required().when("type", {
+    is: "PERCENTAGE",
+    then: Joi.number().max(100).message("Percentage discount cannot exceed 100%"),
+  }),
+  startDate: Joi.date().iso().required(),
+  endDate: Joi.date().iso().greater(Joi.ref("startDate")).required(),
+  minOrderValue: Joi.number().min(0).optional(),
+  maxUsage: Joi.number().integer().min(0).optional(),
+  usageCount: Joi.number().integer().min(0).optional(),
+  perUserLimit: Joi.number().integer().min(0).optional(),
+  applicableSkus: Joi.array().items(Joi.string()).optional(),
+});
+
+const validateDiscount = (req, res, next) => {
+  const { error } = discountSchema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    return res.status(400).json({
+      errors: error.details.map((err) => ({
+        field: err.path.join("."),
+        message: err.message,
+      })),
+    });
+  }
+
+  next();
+};
+
+module.exports = validateDiscount;
